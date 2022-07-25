1bccccc45053923f3f8880dd00c799d7cdd95a55
Prakhar Shrivastava <prakharshri12345@gmail.com>
2022-07-25 22:03:15 +0530

    Add SignupPage and Post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import HomeLayout from './layouts/HomeLayout';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import AddPost from './components/Post/AddPost';
+import Signin from './components/Auth/Signin';
+import Signup from './components/Auth/Signup';
 
 
 function App() {
@@ -14,7 +17,12 @@ function App() {
       <Route path='/:type' element={<HomeLayout>
         <HomePage />
       </HomeLayout>}/>
-      <Route path='/login' element={<LoginPage />} />
+      <Route path='/login' element={<LoginPage>
+        <Signin />
+      </LoginPage>} />
+      <Route path='/signup' element={<LoginPage>
+        <Signup />
+      </LoginPage>} />
     </Routes>
   );
 }
---
+++ client/src/redux/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import image from "./image/image.reducer";
+
+const rootReducer = combineReducers({
+  image,
+});
+
+export default rootReducer;
