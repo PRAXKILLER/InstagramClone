3da1b481ccc3304968f4963cc6ffee5b589df941
Himanshu Singh <78968566+Himanshu1801@users.noreply.github.com>
2022-08-07 13:06:03 +0530

    my work done

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import ProfileSmallScreens from "../components/Profile/ProfileSmallScreens";
 import UserProfileHeader from "../components/Profile/UserProfileHeader";
 
-
 function ProfilePage() {
-    return(
-        <div className="flex flex-col h-full w-full">
-            <UserProfileHeader />
-        </div>
-    )
+  return (
+    <div>
+      <div className="md:flex flex-col h-full w-full hidden">
+        <UserProfileHeader />
+      </div>
+      <div className="md:hidden">
+        <ProfileSmallScreens />
+      </div>
+    </div>
+  );
 }
 
 export default ProfilePage;
